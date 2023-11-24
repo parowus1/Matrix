c15766b3508429dcf63e6780c11e33f76f87d465
Olaf <146707611+parowus1@users.noreply.github.com>
2023-11-24 20:53:21 +0100

    Dodanie konstruktorów i destruktora

--- matrix.cpp
+++ matrix.cpp
@@ -8,3 +8,28 @@ private:
     int** data;
 
 public:
+
+matrix() : size(0), data(nullptr) {}
+matrix(int n) : size(0), data(nullptr) {
+    alokuj(n);
+}
+matrix(int n, int* t) : size(0), data(nullptr) {
+    alokuj(n);
+    for (int i = 0; i < n * n; ++i) {
+        data[i / n][i % n] = t[i];
+    }
+}
+matrix(const matrix& m) : size(0), data(nullptr) {
+    alokuj(m.size);
+    for (int i = 0; i < size * size; ++i) {
+        data[i / size][i % size] = m.data[i / m.size][i % m.size];
+    }
+}
+~matrix() {
+    if (data != nullptr) {
+        for (int i = 0; i < size; ++i) {
+            delete[] data[i];
+        }
+        delete[] data;
+    }
+}

327d84ddb60409c2e0b7466ae579b60ba2684aa8
AdamZiolko <theadamzet@gmail.com>
2023-11-24 21:44:10 +0100

    dodanie  matrix operator++(int)

--- matrix.cpp
+++ matrix.cpp
@@ -386,4 +386,15 @@ matrix& operator-=(int a) {
     return *this;
 }
 
+ matrix operator++(int) {
+        matrix tmp(*this); // Create a copy of the current matrix
+        // Increment each element in the matrix
+        for (int i = 0; i < size; ++i) {
+            for (int j = 0; j < size; ++j) {
+                data[i][j]++;
+            }
+        }
+        return tmp; // Return the original matrix (before increment)
+    }
+
 };
